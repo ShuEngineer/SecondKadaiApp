c1930b2dbf0ef15b62efac02ade95fd4d762d3ff
ShuEngineer <supercar110shu@gmail.com>
2019-11-13 21:33:39 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 伊藤嵩 on 2019/11/13.
+//  Copyright © 2019 Shu Ito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
